93258bca85e823b25b47b8da3de5332ac0809c51
谢庭宇 <qyyzxty@icloud.com>
2021-11-28 10:05:10 +0800

    不知道写了些啥

---
+++ src/力扣刷题/剑指offer/二维数组中的查找/Solution.java
@@ -0,0 +1,64 @@
+package 力扣刷题.剑指offer.二维数组中的查找;
+
+public class Solution {
+    //斜对角线的排除，一行一列的排除
+    //最小的维度
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        //行
+        int m = matrix.length;
+        if (m == 0)
+            return false;
+        //列
+        int n = matrix[0].length;
+
+        for (int i = 0, j = 0; i < m || j < n; i++, j++) {
+            int low, high;
+            if (i == m)
+                i = m - 1;
+            if (j == n)
+                j = n - 1;
+            //二分查找i下面的那一列
+            low = i;
+            high = m - 1;
+            while(low <= high) {
+                int middlei = (low + high) / 2;
+                if (matrix[middlei][j] == target) {
+                    return true;
+                } else if (matrix[middlei][j] > target) {
+                    high = middlei - 1;
+                } else if (matrix[middlei][j] < target) {
+                    low = middlei + 1;
+                }
+            }
+
+            //二分查找j右边的那一行
+            low = j;
+            high = n - 1;
+            while(low <= high) {
+                int middlej = (low + high) / 2;
+                if (matrix[i][middlej] == target) {
+                    return true;
+                } else if (matrix[i][middlej] > target) {
+                    high = middlej - 1;
+                } else if (matrix[i][middlej] < target) {
+                    low = middlej + 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+//        int[][] ma = {
+//            {1,   4,  7, 11, 15},
+//            {2,   5,  8, 12, 19},
+//            {3,   6,  9, 16, 22},
+//            {10, 13, 14, 17, 24},
+//            {18, 21, 23, 26, 30}
+//        };
+        int[][] ma = {
+                {1, 1}
+        };
+        new Solution().findNumberIn2DArray(ma, 0);
+    }
+}
---
+++ src/力扣刷题/剑指offer/从上到下打印二叉树/Solution.java
@@ -0,0 +1,27 @@
+package 力扣刷题.剑指offer.从上到下打印二叉树;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public int[] levelOrder(TreeNode root) {
+        if (root == null)
+            return new int[0];
+        List<TreeNode> list = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            list.add(poll);
+            if (poll.left != null)
+                queue.offer(poll.left);
+            if (poll.right != null)
+                queue.offer(poll.right);
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            result[i] = list.get(i).val;
+        return result;
+    }
+}
