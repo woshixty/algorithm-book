e7813e43142b00b54b9aa9575e06b92c745005c0
谢庭宇 <qyyzxty@icloud.com>
2021-12-06 20:17:40 +0800

    把数字翻译成字符串

---
+++ src/力扣刷题/剑指offer/把数字翻译成字符串/Solution.java
@@ -0,0 +1,44 @@
+package 力扣刷题.剑指offer.把数字翻译成字符串;
+
+public class Solution {
+    int[] a;
+    int length;
+
+    public int translateNum(int num) {
+        a = new int[12];
+        length = 0;
+        if (num == 0) {
+            a[0] = 0;
+            length++;
+        } else {
+            for (int i = 0; num != 0; i++) {
+                a[i] = num % 10;
+                length++;
+                num /= 10;
+            }
+        }
+        return getResult(length - 1);
+    }
+
+    public int getResult(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        if (num == 1) {
+            int result = 10 * a[1] + a[0];
+            if (result<=25 && a[1] != 0)
+                return 2;
+            else
+                return 1;
+        }
+        int result = 10 * a[num] + a[num - 1];
+        if (result <= 25 && a[num] != 0)
+            return getResult(num - 1) + getResult(num - 2);
+        else
+            return getResult(num - 1);
+    }
+
+    public static void main(String[] args) {
+        new Solution().translateNum(506);
+    }
+}
