c445fc1e7098ab0364258751a37a0dec17c2c92c
谢庭宇 <qyyzxty@icloud.com>
2022-01-23 00:31:39 +0800

    剑指offer 22天

--- src/力扣刷题/剑指offer/数组中数字出现的次数/Solution.java
+++ src/力扣刷题/剑指offer/数组中数字出现的次数/Solution.java
@@ -1,6 +1,7 @@
 package 力扣刷题.剑指offer.数组中数字出现的次数;
 
 public class Solution {
+    /**
     public int[] singleNumbers(int[] nums) {
         int n = 0;
         for (int num : nums)
@@ -11,6 +12,25 @@ public class Solution {
         int a = 0, b = 0;
         for (int num : nums) {
             if ((num & position) == 0)
+                a ^= num;
+            else
+                b ^= num;
+        }
+        int[] result = {a, b};
+        return result;
+    }
+     */
+
+    public int[] singleNumbers(int[] nums) {
+        int x = 0;
+        for (int num : nums)
+            x ^= num;
+        int m = 1;
+        while ((x & m) ==0)
+            m <<= 1;
+        int a = 0, b = 0;
+        for (int num : nums) {
+            if ((num & m) == 0)
                 a ^= num;
             else
                 b ^= num;
---
+++ src/力扣刷题/剑指offer/数组中数字出现的次数II/Solution.java
@@ -0,0 +1,19 @@
+package 力扣刷题.剑指offer.数组中数字出现的次数II;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int[] counts = new int[32];
+        for(int num : nums) {
+            for(int j = 0; j < 32; j++) {
+                counts[j] += num & 1;
+                num >>>= 1;
+            }
+        }
+        int res = 0, m = 3;
+        for(int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= counts[31 - i] % m;
+        }
+        return res;
+    }
+}
